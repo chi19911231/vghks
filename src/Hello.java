7a577ef3681afa3f2a9a3f40cc22cc59543edf62
陳廣旂 <kcchen1@vghks.gov.tw>
2025-03-14 09:35:10 +0800

    提交1

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO 自動產生的方法 Stub
 		
-		
 		//---------------------
 		System.out.println("Hello World");
 		System.out.println("Hello VGHKS");
