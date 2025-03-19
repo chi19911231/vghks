0904cc2596c840e98b3e76d0c1a2a223ba9fff24
陳廣旂 <kcchen1@vghks.gov.tw>
2025-03-19 16:55:43 +0800

    commit

--- src/Hello.java
+++ src/Hello.java
@@ -5,11 +5,7 @@ public class Hello {
 		// TODO 自動產生的方法 Stub
 		
 		//---------------------
-<<<<<<< HEAD
-		System.out.println("Hello  Test0001");
-=======
-		System.out.println("Hello  Test0002");
->>>>>>> refs/heads/Test000
+		System.out.println("Hello");
 		//---------------------
 		
 	}
